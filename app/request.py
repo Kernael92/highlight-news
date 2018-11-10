8a9d9a7d80725b1eee7711c28886ef87bf6f096c
Kernael92 <kernaelapuko5@gmail.com>
2018-11-10 23:04:14 +0300

    create a get_article function that takes in the article id and return the article object results

--- app/request.py
+++ app/request.py
@@ -1,8 +1,10 @@
 from app import app
 import urllib.request,json
 from .models import source
+from .models import article
 
 Source = source.Source
+Article = article.Article
 
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
@@ -32,10 +34,10 @@ def process_results(source_list):
     Function  that processes the source result and transform them to a list of Objects
 
     Args:
-        source_list: A list of dictionaries that contain movie details
+        source_list: A list of dictionaries that contain news source details
 
     Returns :
-        source_results: A list of movie objects
+        source_results: A list of source objects
     '''
     source_results = []
     for source_item in source_list:
@@ -67,6 +69,34 @@ def get_articles(id):
             article_results = process_articles(article_results_list)
     
     return article_results
+
+def process_articles(article_list):
+    '''
+    Function  that processes the article result and transform them to a list of Objects
+
+    Args:
+        article_list: A list of dictionaries that contain article details
+
+    Returns :
+        article_results: A list of article objects
+    '''
+    article_results = []
+    for article_item in article_list:
+        id = article_item.get('id')
+        author = article_item.get('author')
+        title = article_item.get('title')
+        description = article_item.get('description')
+        url = article_item.get('url')
+        urlToImage = article_item.get('urlToImage')
+        publishedAt = article_item.get('publishedAt')
+		
+        if urlToImage:
+            article_object = Article(id,author,title,description,url,urlToImage,publishedAt)
+            article_results.append(article_object)	
+    return  article_results
+
+    
+
 
 
 
