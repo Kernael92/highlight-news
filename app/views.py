5ed13c494eb9e6cba1071b3d29c6189a4d006535
Kernael92 <kernaelapuko5@gmail.com>
2018-11-10 15:30:53 +0300

    add HTML structure to the template

--- app/views.py
+++ app/views.py
@@ -7,4 +7,6 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    
+    title = 'Home-Welcome to the best News Article Website online'
+    return render_template('index.html', title=title)
