1d3c74527fc5521ffcd92e0330e545156e7dffaf
Kernael92 <kernaelapuko5@gmail.com>
2018-11-10 16:25:58 +0300

    run a test to confirm that the Source object variables are instantiated

--- test_source.py
+++ test_source.py
@@ -13,5 +13,15 @@ class SourceTest(unittest.TestCase):
         self.new_source = Source('espn','ESPN','ESPN has up-to-the-minute sports news coverage, scores, highlights and commentary for NFL, MLB, NBA, College Football, NCAA Basketball and more.','http://espn.go.com','sports','en','us')
     def test_instance(self):
         self.assertTrue(isinstance(self.new_source,Source))
+
+
+
+    def test_check_instance_variables(self):
+        self.assertEquals(self.new_source_id,'espn')
+        self.assertEquals(self.new_source_name,'ESPN')
+        self.assertEquals(self.new_source_description,'ESPN has up-to-the-minute sports news coverage, scores, highlights and commentary for NFL, MLB, NBA, College Football, NCAA Basketball and more.')
+        self.assertEquals(self.new_source_url,'http://espn.go.com')
+        self.assertEquals(self.new_source_country,'us')
+        self.assertEquals(self.new_source_language,'en')
 if __name__ == '__main__':
     unittest.main()
