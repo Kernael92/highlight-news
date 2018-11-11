3eb6c0072534b3ceef5b5fdd83a85ce9730276c6
Kernael92 <kernaelapuko5@gmail.com>
2018-11-12 01:22:47 +0300

    create and registera Blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,26 @@
 from flask import Flask
-from .config import DevConfig
-from flask_bootstrap import Bootstrap  
+from config import config_options
+from flask_bootstrap import Bootstrap
 
-# Initializing application
-app = Flask(__name__, instance_relative_config = True)
+bootstrap = Bootstrap()
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+def create_app(config_name):
 
-# Initializing Flask Extensions
-bootstrap = Bootstrap(app)
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing application
+    bootstrap.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
 
-from app import views
-from app import error
+    
+
+
+    return app
 
--- config.py
+++ config.py
@@ -8,6 +8,9 @@ class Config:
 
     NEWS_SOURCES_BASE_URL = 'http://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
     ARTICLES_BASE_URL = 'http://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    
 
 
 class ProdConfig(Config):
@@ -29,3 +32,8 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
